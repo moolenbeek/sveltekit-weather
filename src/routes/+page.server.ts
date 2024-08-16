5e91da08ae68468c82b0f3ec3a845101fa1f594e
moolenbeek <emoolenbeek@gmail.com>
2024-08-15 22:19:14 -0400

    format

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,16 +1,16 @@
-import type { PageServerLoad } from './$types';
-import { WEATHER_API_KEY } from '$env/static/private';
+import type { PageServerLoad } from "./$types";
+import { WEATHER_API_KEY } from "$env/static/private";
 
 const lat = "45.424721";
 const lon = "-75.695000";
 const part = "current";
-const units = "metric"
+const units = "metric";
 
 export const load = (async ({ fetch }) => {
 	const res = await fetch(
 		`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${WEATHER_API_KEY}&units=${units}`,
 		{
-			method: 'GET'
+			method: "GET"
 		}
 	);
 
