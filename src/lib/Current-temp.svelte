5245af3505937580d0435d267b4f1d508023df99
moolenbeek <emoolenbeek@gmail.com>
2024-08-16 07:47:05 -0400

    refactor

--- src/lib/Current-temp.svelte
+++ src/lib/Current-temp.svelte
@@ -6,10 +6,43 @@
 	let temp = data.props.data.hourly[0].temp;
 	let feelsLike = data.props.data.hourly[0].feels_like;
 	let description = data.props.data.hourly[0].weather[0].description;
+	let icon = data.props.data.hourly[0].weather[0].icon;
 </script>
 
-{#if data !== ""}
-	<p>{description}</p>
-	<p>Temperature {temp}</p>
-	<p>Feels Like {feelsLike}</p>
-{/if}
+<div id="container">
+	{#if data !== ""}
+    <h1>Ottawa, ON</h1>
+    <img id="icon" src="https://openweathermap.org/img/wn/{icon}@2x.png" alt="">
+		<p>{description}</p>
+		<p>Temperature {temp}</p>
+		<p>Feels Like {feelsLike}</p>
+	{/if}
+</div>
+
+<style>
+ #container {
+  background-color: #9286f0;
+  border-radius: 30px;
+  padding: 1em;
+  margin: 1em;
+
+  box-shadow: 0px 5px 5px 0px rgba(0,0,0,0.1);
+ }
+ h1 {
+  text-align: center;
+  color:#ffffe4;
+  font-family: "archivo", sans-serif;
+  font-size: 2em;
+ }
+ p {
+  text-align: center;
+  color: #ffffe4;
+  font-family: "open-sans", sans-serif;
+  font-size: 1em;
+ }
+ #icon {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+ }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,4 @@
 	export let data;
 </script>
 
-<h1 id="title">Weather App</h1>
 <CurrentTemp {data}/>
-
-<style>
-	#title {
-		color: Black;
-		font-family: "arimo", sans-serif;
-		font-size: 2em;
-	}
-</style>
