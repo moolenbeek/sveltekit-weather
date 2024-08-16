87b3043420dc42b77c5bc33c874ff4376ffaeb55
moolenbeek <emoolenbeek@gmail.com>
2024-08-15 21:14:28 -0400

    rename weather API key variable

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,5 @@
 import type { PageServerLoad } from './$types';
-import { API_KEY } from '$env/static/private';
-
-const test = "testing ...";
+import { WEATHER_API_KEY } from '$env/static/private';
 
 const lat = "45.424721";
 const lon = "-75.695000";
@@ -10,7 +8,7 @@ const units = "metric"
 
 export const load = (async ({ fetch }) => {
 	const res = await fetch(
-		`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${API_KEY}&units=${units}`,
+		`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${WEATHER_API_KEY}&units=${units}`,
 		{
 			method: 'GET'
 		}
