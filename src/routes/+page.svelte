374fa25597f99ed7f7efecebb7df042ddf38dff1
moolenbeek <emoolenbeek@gmail.com>
2024-08-15 21:14:53 -0400

    display current temp and feels like

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,17 @@
 	/** @type {import('./$types').PageData} */
 	export let data;
 	console.log(data);
+
+  let temp = data.props.data.hourly[0].temp;
+  let feelsLike = data.props.data.hourly[0].feels_like;
 </script>
 
 <h1 id="title">Weather App</h1>
+
+{#if temp !== ""}
+  <p>Temperature {temp}</p>
+  <p>Feels Like {feelsLike}</p>
+{/if}
 
 <style>
 	#title {
