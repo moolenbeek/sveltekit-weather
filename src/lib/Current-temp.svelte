da95a3e62277e3d92bd81986aa3e3c98b9cea4ea
moolenbeek <emoolenbeek@gmail.com>
2024-08-15 22:17:14 -0400

    create component

---
+++ src/lib/Current-temp.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	/** @type {import('./$types').PageData} */
+	export let data;
+	console.log(data);
+
+	let temp = data.props.data.hourly[0].temp;
+	let feelsLike = data.props.data.hourly[0].feels_like;
+	let description = data.props.data.hourly[0].weather[0].description;
+</script>
+
+{#if data !== ""}
+	<p>{description}</p>
+	<p>Temperature {temp}</p>
+	<p>Feels Like {feelsLike}</p>
+{/if}
